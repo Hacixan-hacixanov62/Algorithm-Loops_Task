b32ddaebeafaa876ec333db8606664a067fe4223
unknown <hajikhanih@code.edu.az>
2024-10-21 00:48:30 +0400

    feat; added Task folder

---
+++ Algoritms-Loop_Task/Program.cs
@@ -0,0 +1,113 @@
+﻿//HomeWork
+
+//Task:
+//Aşağıdakı tapşırığı flochart və pseudocode ilə yazın.
+
+
+//Verilmiş n ve m (n<m) ədədləri arasındakı tək ədədlərin sayını tapın.
+//Verilmiş n ve m (n<m) ededleri arasindaki tek ədədlərin cəmini tapin.
+//m -ədədi verilir. 1-dən m-ədədinə kimi olan cüt ədədlərin hasilini tapin.
+//a - ədədi 3-ə bölünən cüt ədəddirsə "3-e bolunur cutdur" ,3-ə bölünən tək ədəddirsə " ,3-e bolunur tekdir" əks halda "3-e bolunmur" sözünü ekranda çap edin.
+//Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+
+
+//Verilmiş n ve m (n<m) ədədləri arasındakı tək ədədlərin sayını tapın.
+
+//int m = 10;
+//int n = 2;
+//int count = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//Verilmiş n ve m (n<m) ededleri arasindaki tek ədədlərin cəmini tapin.
+
+//int m = 10;
+//int n = 2;
+//int sum = 0;
+
+//for (int i = n; i < m; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        sum += i; 
+//    }
+//}
+//Console.WriteLine(sum);
+
+//m -ədədi verilir. 1-dən m-ədədinə kimi olan cüt ədədlərin hasilini tapin.
+
+//int m = 10;
+//int resoult = 1;
+
+//for (int i = 1; i < m; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        resoult *= i;
+//    }
+//}
+//Console.WriteLine(resoult);
+
+//a - ədədi 3-ə bölünən cüt ədəddirsə "3-e bolunur cutdur" ,3-ə bölünən tək ədəddirsə " ,3-e bolunur tekdir" əks halda "3-e bolunmur" sözünü ekranda çap edin.
+
+//void Number()
+//{
+//    Console.Write("Enter a number: ");
+//    int a = Convert.ToInt32(Console.ReadLine());
+
+//    if (a % 3 == 0) 
+//    {
+//        if (a % 2 == 0) 
+//        {
+//            Console.WriteLine(" 3-e bolunen cutdur");
+//        }
+//        else 
+//        {
+//            Console.WriteLine(" 3-e bolunen tekdir");
+//        }
+//    }
+//    else 
+//    {
+//        Console.WriteLine(" 3-e bolunmur");
+//    }
+//}
+
+//Number();
+
+//Verilmiş n tam ədədinin sadə və ya mürəkkəb ədəd olduğunu tapın.
+
+//int n = 1;
+//int count = 0;
+
+//if (n < 2)
+//{
+//    Console.WriteLine("ne sade,ne murekkeb ededir");
+//}
+//else
+//{
+//	for (int i = 1; i <=n ; i++)
+//	{
+//		if (n%i == 0)
+//		{
+//			count++;
+//		}
+//	}
+//}
+//if(count == 2)
+//{
+//    Console.WriteLine("sade");
+//}
+//else
+//{
+//    Console.WriteLine("murekkeb");
+//}
+
+
+
